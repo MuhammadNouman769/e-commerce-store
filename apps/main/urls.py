130c5b1be5ccf488cd04f23ff908eb425227b219
Usama Shafaqat <usama.shafaqat@ginkgoretail.com>
2025-12-09 00:23:03 +0500

    design fully set up by Muhammad Nouman

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('women/', views.women, name='women'),
+    path('about_us/' ,views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('men/', views.men, name='men'),
+    path('products_details/', views.products_details, name='products_details'),
+   path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('order_complete/', views.order_complete, name='order_complete'),
+    path('wishlist/', views.wishlist, name='wishlist'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -7,4 +7,20 @@ def home(request):
      
 def women(request):
      return render(request, 'products/women.html')
-          
+
+def about_us(request):
+    return render(request, 'about/about.html')
+def contact(request):
+    return render(request, 'contacts/contact.html')
+def men(request):
+    return render(request, 'products/men.html')
+def products_details(request):
+    return render(request, 'products/product-detail.html')
+def cart(request):
+    return render(request, 'cart/cart.html')
+def checkout(request):
+    return render(request, 'check_out/checkout.html')
+def order_complete(request):
+    return render(request, 'order_complete/order-complete.html')
+def wishlist(request):
+    return render(request, 'wish-list/add-to-wishlist.html')
