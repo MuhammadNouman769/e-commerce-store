9da32295fa61592d4f815f81ef8262f596ad5977
Muhammad Nouman <nomannisar769@gmail.com>
2026-03-03 16:11:58 +0500

    update txt file

--- apps/products/serializer.py
+++ apps/products/serializer.py
@@ -4,12 +4,12 @@ from rest_framework import serializers
 
 ''' ===================== Category Serializer ===================== '''
 class CategorySerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+    #image_url = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = ["id", "name", "slug", "parent", "is_active", "position", "description", "image_url"]
-    def get_image_url(self,  obj):
-        request = self.context.get('request')
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
+        fields = ["id", "name", "slug", "parent", "is_active", "position", "description", "image"]
+    # def get_image_url(self,  obj):
+    #     request = self.context.get('request')
+    #     if obj.image and request:
+    #         return request.build_absolute_uri(obj.image.url)
+    #     return None
