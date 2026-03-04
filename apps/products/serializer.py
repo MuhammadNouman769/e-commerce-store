bd2b5a0504d276d7d7596322037fcebdaca86dc1
Muhammad Nouman <nomannisar769@gmail.com>
2026-03-04 13:29:30 +0500

    create products and products details api list and crud serializers

--- apps/products/serializer.py
+++ apps/products/serializer.py
@@ -1,15 +1,29 @@
 from .models import Category
 from rest_framework import serializers
 
+''' ===================== Category Childern Serializer ===================== '''
+class CategoryChildernSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 
 ''' ===================== Category Serializer ===================== '''
 class CategorySerializer(serializers.ModelSerializer):
-    #image_url = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = ["id", "name", "slug", "parent", "is_active", "position", "description", "image"]
-    # def get_image_url(self,  obj):
-    #     request = self.context.get('request')
-    #     if obj.image and request:
-    #         return request.build_absolute_uri(obj.image.url)
-    #     return None
+        fields = ['id', 'name', 'slug', 'parent',  'position', 'children', 'shop']
+        read_only_fields = ["slug"]
+
+class RecursiveCategorySerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug', 'parent',  'position', 'children', 'shop']
+        read_only_fields = ["slug"]
+
+    def get_children(self, obj):
+        if obj.children.exists():
+            return RecursiveCategorySerializer(obj.children.all(), many=True).data
+        return []
