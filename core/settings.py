47c2e814497b03db8a6befc8d79cfbc1d5282238
Muhammad Nouman <nomannisar769@gmail.com>
2026-03-05 14:32:45 +0500

    templates import

--- core/settings.py
+++ core/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "templates/../static"]
 
 # Media files (for uploaded images like profile pictures)
 MEDIA_URL = '/media/'
