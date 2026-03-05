a327f6f9c3256902afba619bd3dc6786ee1b7953
Muhammad Nouman <nomannisar769@gmail.com>
2026-03-05 16:17:19 +0500

    template load starting

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from apps.main.models import Banners
 
 
 def home(request):
-    banners = Banners.objects.filter(is_active=True)
-    return render(request, 'home/index.html', {"banners": banners})
+    return render(request, 'index.html')
      
      
 
 
-def about_us(request):
-    return render(request, 'about/about.html')
-def contact(request):
-    return render(request, 'contacts/contact.html')
-def men(request):
-    return render(request, 'products/men.html')
-def products_details(request):
-    return render(request, 'products/product-detail.html')
-def cart(request):
-    return render(request, 'cart/cart.html')
-def checkout(request):
-    return render(request, 'check_out/checkout.html')
-def order_complete(request):
-    return render(request, 'order_complete/order-complete.html')
-def wishlist(request):
-    return render(request, 'wish-list/add-to-wishlist.html')
+# def about_us(request):
+#     return render(request, 'about/about.html')
+# def contact(request):
+#     return render(request, 'contacts/contact.html')
+# def men(request):
+#     return render(request, 'products/men.html')
+# def products_details(request):
+#     return render(request, 'products/product-detail.html')
+# def cart(request):
+#     return render(request, 'cart/cart.html')
+# def checkout(request):
+#     return render(request, 'check_out/checkout.html')
+# def order_complete(request):
+#     return render(request, 'order_complete/order-complete.html')
+# def wishlist(request):
+#     return render(request, 'wish-list/add-to-wishlist.html')
 
-def login(request):
-    return render(request, 'users/login.html')
+# def login(request):
+#     return render(request, 'users/login.html')
