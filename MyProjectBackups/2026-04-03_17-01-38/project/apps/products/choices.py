93d7470166bcc04d73127142fd41c3dcccb216ea
Muhammad Nouman <nomannisar769@gmail.com>
2026-04-08 13:18:01 +0500

    delete backup

--- MyProjectBackups/2026-04-03_17-01-38/project/apps/products/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class ProductStatus(models.TextChoices):
-    DRAFT = "draft", "Draft"
-    ACTIVE = "active", "Active"
-    ARCHIVED = "archived", "Archived"
-    OUT_OF_STOCK = "out_of_stock", "Out of Stock"
